184a70e179d93b3c07560efc15575d40695a907e
Wojciech Lukaszewicz <voytel@gmail.com>
2019-11-05 17:37:44 +1000

    Fixed Browser History warning

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
-import createHistory from 'history/createBrowserHistory'
+// import createHistory from 'history/createBrowserHistory'
+import { createBrowserHistory } from 'history'
 import DashboardPage from '../components/DashboardPage'
 import NotFoundPage from '../components/NotFoundPage'
 import LoginPage from '../components/LoginPage'
 import PrivateRoute from './PrivateRoute'
 import PublicRoute from './PublicRoute'
 
-export const history = createHistory()
+export const history = createBrowserHistory()
 
 const AppRouter = () => (
   <Router history={history}>
